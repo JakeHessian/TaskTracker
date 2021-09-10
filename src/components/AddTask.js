c12e0883122f690e455ff72af92aba65f181174b
Jake Hessian <jake.hessian@gmail.com>
2021-09-10 14:30:39 -0400

    added usestate functions to form inputs

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,6 +1,8 @@
-import { FaBlackberry } from "react-icons/fa";
-
+import { useState } from "react";
 const AddTask = () => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
   const buttonStyle = {
     //display: block,
     width: "100%",
@@ -13,13 +15,27 @@ const AddTask = () => {
     <form className="add-form">
       <div className="form-control">
         <label>Task</label>
-        <input type="text" placeholder="Add Task"></input>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Add Task"
+        ></input>
       </div>
       <div className="form-control">
         <label>Day and Time</label>
-        <input type="text" placeholder="Add Day and Time"></input>
+        <input
+          type="text"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+          placeholder="Add Day and Time"
+        ></input>
       </div>
-      <div className="form-control form-control-check">
+      <div
+        className="form-control form-control-check"
+        onChange={(e) => setText(e.currentTarget.checked)}
+        value={reminder}
+      >
         <label>Set Time</label>
         <input type="checkbox"></input>
       </div>
