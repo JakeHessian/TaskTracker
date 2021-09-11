f43235c5efc6ee5f236473323d7bca9290fa9263
Jake Hessian <jake.hessian@gmail.com>
2021-09-11 11:53:13 -0400

    Add/close button changes text based on state

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,13 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
-  const onClick = () => {
-    console.log("Click!~");
-  };
   return (
     <header className="header">
       <h1>{title}</h1>
       <Button
         background_color="Blue"
         text_color="white"
-        text="Add"
+        text={showAdd ? "Close" : "Add"}
         onClick={onAdd}
       />
     </header>
